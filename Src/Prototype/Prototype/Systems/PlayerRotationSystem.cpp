305bd835e7e2e96df520ff5cc8b7f923f22e4ffd
alexcheero <alexcheero157@gmail.com>
2019-12-12 17:54:50 +0300

    PlayerRotationSystem

--- Src/Prototype/Prototype/Systems/PlayerRotationSystem.cpp
+++ Src/Prototype/Prototype/Systems/PlayerRotationSystem.cpp
@@ -1,30 +1,33 @@
 #include "PlayerRotationSystem.h"
 
 #include <Transform.hpp>
+#include <Camera.hpp>
 
 #include "../Components/Player.h"
 
 void godot::PlayerRotationSystem::operator()(float delta, entt::registry& registry)
 {
 	Input* pInput = Input::get_singleton();
-	//int mask = 0;
-	//if (pInput->is_action_pressed("ui_left"))
-	//	mask |= 1;
-	//if (pInput->is_action_pressed("ui_right"))
-	//	mask |= 1 << 1;
-	//if (pInput->is_action_pressed("ui_up"))
-	//	mask |= 1 << 2;
-	//if (pInput->is_action_pressed("ui_down"))
-	//	mask |= 1 << 3;
+	//TODO: check captures of all systems so far
+	registry.view<RotationComponent, Player*, Camera*>().each([&](RotationComponent rotationComp, Player* pPlayer, Camera* pCam)
+	{
+		Basis camBasis = pCam->get_global_transform().get_basis();
+		Vector3 dir{ 0, 0, 0 };
+		if (pInput->is_action_pressed("ui_left"))
+			dir += camBasis.x;
+		if (pInput->is_action_pressed("ui_right"))
+			dir -= camBasis.x;
+		if (pInput->is_action_pressed("ui_up"))
+			dir += camBasis.z;
+		if (pInput->is_action_pressed("ui_down"))
+			dir -= camBasis.z;
+		dir.y = 0;
 
-	if (pInput->is_action_just_pressed("ui_left"))		
-		//TODO: check captures of all systems so far
-		registry.view<RotationComponent, Player*>().each([&](RotationComponent rotationComp, Player* pPlayer)
-		{
-			Spatial* camSpatial = Object::cast_to<Spatial>(pPlayer->get_node("Camera"));
-			Transform camTransform = camSpatial->get_global_transform();
-			Vector3 playerPosition = pPlayer->get_global_transform().get_origin();
-			Vector3 lookTarget = playerPosition + camTransform.get_basis().x * 10;
-			pPlayer->set_transform(pPlayer->get_transform().looking_at(Vector3{}, Vector3{ 0, 1, 0 }));
-		});
+		if (dir.length_squared() == 0)
+			return;
+		
+		Vector3 playerPosition = pPlayer->get_global_transform().get_origin();
+		Vector3 lookTarget = playerPosition - dir;
+		pPlayer->look_at(lookTarget, Vector3{ 0, 1, 0 });
+	});
 }
