0c9ce5d687ca39b30f6c5ee412b1f735070d4158
alexcheero157 <alexcheero157@gmail.com>
2019-12-16 03:08:10 +0300

    AttackSystem refactoring, but with cleanup warning

--- Src/Prototype/Prototype/Systems/AttackSystem.cpp
+++ Src/Prototype/Prototype/Systems/AttackSystem.cpp
@@ -4,14 +4,31 @@
 #include <World.hpp>
 #include <PhysicsDirectSpaceState.hpp>
 #include <Dictionary.hpp>
-#include <PhysicsShapeQueryParameters.hpp>
 #include <SphereShape.hpp>
 
-#include <string>
+//#include <string>
 
 #include "core/math/math_funcs.h"
 
 #include "../Components/Enemy.h"
+
+const float INTERSECT_RESULTS_NUM = 16.f;
+
+godot::AttackSystem::AttackSystem()
+{
+	//WARNING: cleanup: ObjectDB Instances still exist!
+	//	  At: core / object.cpp : 2087
+	m_params = (Ref<PhysicsShapeQueryParameters>)PhysicsShapeQueryParameters::_new();
+	m_params->set_collision_mask(1 << 2);//Enemy layer TODO: make util to get mask/layer by name
+	m_params->set_collide_with_areas(false);
+	m_params->set_collide_with_bodies(true);
+}
+
+godot::AttackSystem::~AttackSystem()
+{
+	std::cout << "~AttackSystem";
+	m_params->free();
+}
 
 void godot::AttackSystem::operator()(float delta, entt::registry& registry)
 {
@@ -19,19 +36,15 @@ void godot::AttackSystem::operator()(float delta, entt::registry& registry)
 	if (!pInput->is_action_just_pressed("attack"))
 		return;
 
-	registry.view<AttackComponent, Spatial*>().each([&registry](AttackComponent attackComp, Spatial* pAttackerSpatial)
+	registry.view<AttackComponent, Spatial*>().each([&registry, this](AttackComponent attackComp, Spatial* pAttackerSpatial)
 	{
 		PhysicsDirectSpaceState* spaceState = pAttackerSpatial->get_world()->get_direct_space_state();
-		Ref<PhysicsShapeQueryParameters> params = PhysicsShapeQueryParameters::_new();//TODO: cache
 		Ref<SphereShape> shape = SphereShape::_new();
 		shape->set_radius(attackComp.distance);
-		params->set_shape(shape);
-		params->set_collision_mask(1 << 2);//Enemy layer TODO: make util to get mask/layer by name
-		params->set_collide_with_areas(false);
-		params->set_collide_with_bodies(true);
+		m_params->set_shape(shape);
 		Transform attackerTransform = pAttackerSpatial->get_global_transform();
-		params->set_transform(attackerTransform);
-		Array intersects = spaceState->intersect_shape(params, 16);//TODO: hardcode
+		m_params->set_transform(attackerTransform);
+		Array intersects = spaceState->intersect_shape(m_params, INTERSECT_RESULTS_NUM);
 
 		if (intersects.size() == 0)
 			return;
@@ -39,15 +52,8 @@ void godot::AttackSystem::operator()(float delta, entt::registry& registry)
 		Dictionary dict = intersects[0];//TODO: hits only first intersected, implement area hits
 		Object* pObj = Node::___get_from_variant(dict["collider"]);
 		
-		//TODO: check attack angle
-		Spatial* pEnemySpatial = Object::cast_to<Spatial>(pObj);
-		Vector3 enemyPosition = pEnemySpatial->get_global_transform().origin;
-		Vector3 toEnemyDirection = enemyPosition - attackerTransform.origin;
-		toEnemyDirection.normalize();
-		float angleCos = toEnemyDirection.dot(-attackerTransform.basis.z);
-		float angle = Math::rad2deg(Math::acos(angleCos));
-
-		if (angle > attackComp.angle)
+		Vector3 enemyPosition = Object::cast_to<Spatial>(pObj)->get_global_transform().origin;
+		if (!CheckAttackAngle(attackerTransform, enemyPosition, attackComp.angle))
 			return;
 
 		Enemy* pEnemy = Object::cast_to<Enemy>(pObj);
@@ -55,10 +61,24 @@ void godot::AttackSystem::operator()(float delta, entt::registry& registry)
 		
 		HealthComponent& enemyHealthComp = registry.get<HealthComponent>(enemyEntity);
 		enemyHealthComp.hp -= attackComp.damage;
+
 		if (enemyHealthComp.hp <= 0)
 		{
+			Godot::print("Killed!");
 			enemyHealthComp.hp = 0;
 			registry.assign<DeadComponent>(enemyEntity);
 		}
+		else
+			Godot::print("Hit!");
 	});
+}
+
+bool godot::AttackSystem::CheckAttackAngle(Transform attackerTransform, Vector3 enemyPosition, float maxAngle)
+{
+	Vector3 toEnemyDirection = enemyPosition - attackerTransform.origin;
+	toEnemyDirection.normalize();
+	float angleCos = toEnemyDirection.dot(-attackerTransform.basis.z);
+	float angle = Math::rad2deg(Math::acos(angleCos));
+
+	return angle <= maxAngle;
 }
--- Src/Prototype/Prototype/Systems/AttackSystem.h
+++ Src/Prototype/Prototype/Systems/AttackSystem.h
@@ -3,13 +3,19 @@
 #include "BaseSystem.h"
 #include "../Components/SimpleComponents.h"
 
+#include <PhysicsShapeQueryParameters.hpp>
+
 namespace godot
 {
 	//TODO: rename to PlayerAttackSystem or abstract player input somehow
 	//TODO: test or remove testable
 	class TESTABLE_CLASS AttackSystem : public BaseSystem
 	{
+		Ref<PhysicsShapeQueryParameters> m_params;
 	public:
+		AttackSystem();
+		~AttackSystem();
 		virtual void operator()(float delta, entt::registry& registry) override;
+		bool CheckAttackAngle(Transform attackerTransform, Vector3 enemyPosition, float maxAngle);
 	};
 }
