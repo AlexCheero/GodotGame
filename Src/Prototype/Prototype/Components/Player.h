06d05c321c1468e907958d3aa63db87b40802668
alexcheero157 <alexcheero157@gmail.com>
2019-12-22 02:46:53 +0300

    todo

--- Src/Prototype/Prototype/Components/Player.h
+++ Src/Prototype/Prototype/Components/Player.h
@@ -6,6 +6,7 @@
 namespace godot
 {
 	//TODO: show fields of godot-related components in editor
+	//TODO: create somekind of godot-entity view, to show which components are assigned to entity
 	class Player : public KinematicBody
 	{
 		GODOT_CLASS(Player, KinematicBody)
