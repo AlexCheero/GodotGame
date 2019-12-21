539a49de0f57e60b2defb1140f246b1fdede092d
alexcheero157 <alexcheero157@gmail.com>
2019-12-22 02:41:14 +0300

    one more todo

--- Src/Prototype/Prototype/Systems/PlayerInputSystem.h
+++ Src/Prototype/Prototype/Systems/PlayerInputSystem.h
@@ -9,6 +9,7 @@ namespace godot
 	//TODO: change name not to confuse it with classes that are inherited from base system
 	class PlayerInputSystem
 	{
+		//TODO: come up with what to do with static methods
 		static void GetInputDirection(Vector2& dir, InputEvent* e, const char* actionPrefix);
 	public:
 		void operator()(entt::registry& registry, InputEvent* e);
