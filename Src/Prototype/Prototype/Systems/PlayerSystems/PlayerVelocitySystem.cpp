7aa91de7c92366d15ac7a0994606f0fce1595fcb
alexcheero <alexcheero157@gmail.com>
2020-02-05 16:02:47 +0300

    hardcoded movement animations

--- Src/Prototype/Prototype/Systems/PlayerSystems/PlayerVelocitySystem.cpp
+++ Src/Prototype/Prototype/Systems/PlayerSystems/PlayerVelocitySystem.cpp
@@ -5,6 +5,12 @@
 
 #include "../../Components/InputComponents.h"
 
+//TODO: move anim code to different system
+#include "../../Components/SimpleComponents.h"
+#include "../../Utils/Utils.h"
+#include "core/math/math_funcs.h"
+//----------------------------------------
+
 void godot::PlayerVelocitySystem::Update(VelocityComponent& velocityComp, SpeedComponent speedComp, Basis camBasis, Vector2 input)
 {
 	Vector3 flatVelocity = Vector3(0, 0, 0);
@@ -20,12 +26,62 @@ void godot::PlayerVelocitySystem::Update(VelocityComponent& velocityComp, SpeedC
 
 void godot::PlayerVelocitySystem::operator()(float delta, entt::registry& registry)
 {
-	auto view = registry.view<entt::tag<PlayerTag>, VelocityComponent, InputComponent, SpeedComponent, Camera*>();
-	view.less([&registry](entt::entity entity, VelocityComponent& velocity, InputComponent input, SpeedComponent speedComp, Camera* pCam)
+	auto view = registry.view<entt::tag<PlayerTag>, VelocityComponent, InputComponent, SpeedComponent, Camera*, RotationDirectionComponent, AnimationTree*>();
+	view.less([&registry](entt::entity entity, VelocityComponent& velocityComp, InputComponent input
+		, SpeedComponent speedComp, Camera* pCam,/*anim params starts from here*/ RotationDirectionComponent rotationComp, AnimationTree* pAnimTree)
 	{
-		if (registry.has<AnimationTree*>(entity))
-			registry.get<AnimationTree*>(entity)->set("parameters/BlendSpace2D/blend_position", input.moveDir);
+		Update(velocityComp, speedComp, pCam->get_transform().get_basis(), input.moveDir);
 
-		Update(velocity, speedComp, pCam->get_transform().get_basis(), input.moveDir);
+		//TODO: move anim code to different system
+		bool right = velocityComp.velocity.cross(rotationComp.direction).y < 0;
+		//TODO: angle isn't precise cause velocity's y
+		//float angle = Math::acos(velocityComp.velocity.normalized().dot(rotationComp.direction));
+		//if (right)
+		//	angle *= -1;
+		
+		//float x = Math::cos(angle) * input.moveDir.x - Math::sin(angle) * input.moveDir.y;
+		//float y = Math::sin(angle) * input.moveDir.x + Math::cos(angle) * input.moveDir.y;
+		//Vector2 blend_pos = Vector2{ x, y }.normalized();
+		//pAnimTree->set("parameters/BlendSpace2D/blend_position", blend_pos);
+
+		float angle = Math::rad2deg(Math::acos(velocityComp.velocity.normalized().dot(rotationComp.direction)));
+		if (right)
+		{
+			angle = 360 - angle;
+			//angle *= -1;
+		}
+
+		//TODO: diagonal animations are wrong, probably because of blending
+		Vector2 blend_pos;
+		//no movement
+		if (input.moveDir.length_squared() == 0)
+			blend_pos = Vector2{ 0, 0 };
+		//fwd + rght
+		else if (angle > 45 - 22.5f && angle <= 45 + 22.5f)
+			blend_pos = Vector2{ 1, -1 }.normalized();
+		//rght
+		else if (angle > 90 - 22.5f && angle <= 90 + 22.5f)
+			blend_pos = Vector2{ 1, 0 };
+		//bwd + rght
+		else if (angle > 135 - 22.5f && angle <= 135 + 22.5f)
+			blend_pos = Vector2{ 1, 1 }.normalized();
+		//bwd
+		else if (angle > 180 - 22.5f && angle <= 180 + 22.5f)
+			blend_pos = Vector2{ 0, 1 }.normalized();
+		//bwd + lft
+		else if (angle > 225 - 22.5f && angle <= 225 + 22.5f)
+			blend_pos = Vector2{ -1, 1 }.normalized();
+		//lft
+		else if (angle > 270 - 22.5f && angle <= 270 + 22.5f)
+			blend_pos = Vector2{ -1, 0 }.normalized();
+		//fwd + lft
+		else if (angle > 315 - 22.5f && angle <= 315 + 22.5f)
+			blend_pos = Vector2{ -1, -1 }.normalized();
+		//fwd
+		else if (angle > 315 + 22.5f || angle <= 0 + 22.5f)
+			blend_pos = Vector2{ 0, -1 };
+
+		pAnimTree->set("parameters/BlendSpace2D/blend_position", blend_pos);
+		//----------------------------------------
 	});
 }
