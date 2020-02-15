03ebd15bf5af5bd0c2ccec9deacd7e7c2edc4c92
alexcheero157 <alexcheero157@gmail.com>
2020-02-15 23:42:33 +0300

    addition to prev commit

--- Src/Prototype/Prototype/Nodes/HTHDamagingArea.cpp
+++ Src/Prototype/Prototype/Nodes/HTHDamagingArea.cpp
@@ -5,14 +5,10 @@
 void godot::HTHDamagingArea::_register_methods()
 {
 	register_method((char*)"_on_Area_body_entered", &HTHDamagingArea::_on_Area_body_entered);
-
-	register_signal<HTHDamagingArea>("hth_hit", "hitted_entity", GODOT_VARIANT_TYPE_INT);
 }
 
 void godot::HTHDamagingArea::_on_Area_body_entered(EntityHolderNode* pEntityHolder)
 {
 	if (!pEntityHolder)
 		return;
-
-	emit_signal("hth_hit", (int)pEntityHolder->GetEntity());
 }
