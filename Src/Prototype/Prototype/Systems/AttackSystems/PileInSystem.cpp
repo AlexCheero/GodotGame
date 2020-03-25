142136de2512cfb0eb1657eac3e9a9721cd26197
alexcheero <alexcheero157@gmail.com>
2020-03-25 21:51:31 +0300

    PileInSystem refactoring

--- Src/Prototype/Prototype/Systems/AttackSystems/PileInSystem.cpp
+++ Src/Prototype/Prototype/Systems/AttackSystems/PileInSystem.cpp
@@ -7,10 +7,8 @@ void godot::PileInSystem::operator()(float delta, entt::registry& registry)
 	//TODO: remove hardcode and calculate separate maxAttackDistance for each hit
 	//TODO: player + bot colliders radii is hardcoded also
 	const float maxAttackDistance = 2.f;//1.5f
-	auto checkForPileInView = registry.view<entt::tag<AttackActionTag>, TargetLockComponent,
-											VelocityComponent, SpeedComponent, Spatial*>(entt::exclude<entt::tag<PileInTag> >);
-	checkForPileInView.less([&registry, maxAttackDistance](entt::entity entity, TargetLockComponent lockComp, VelocityComponent velComp,
-														   SpeedComponent speedComp, Spatial* pSpatial)
+	auto checkForPileInView = registry.view<entt::tag<AttackActionTag>, TargetLockComponent, Spatial*>(entt::exclude<entt::tag<PileInTag> >);
+	checkForPileInView.less([&registry, maxAttackDistance](entt::entity entity, TargetLockComponent lockComp, Spatial* pSpatial)
 	{
 		ASSERT(registry.has<Spatial*>(lockComp.target), "target has no spatial");
 		Spatial* pTargetSpatial = registry.get<Spatial*>(lockComp.target);
