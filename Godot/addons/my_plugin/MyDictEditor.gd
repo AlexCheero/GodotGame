471e5e8d4e53fd42a833315d9ab1ea4f91a15271
alexcheero <alexcheero157@gmail.com>
2020-04-08 14:18:22 +0300

    keep working on entity inspector

--- Godot/addons/my_plugin/MyDictEditor.gd
+++ Godot/addons/my_plugin/MyDictEditor.gd
@@ -1,21 +1,28 @@
+tool
 extends EditorProperty
 class_name MyDictEditor
 
 var expandBtn = Button.new()
 var entry = HBoxContainer.new()
 
-func _init():
+func _ready():
+	print("_init")
 	expandBtn.text = "Show"
 	expandBtn.connect("pressed", self, "_on_expand_pressed")
 	
 	# set components in the loop
 	var componentName = Label.new()
 	componentName.align = Label.ALIGN_LEFT
-	componentName.size_flags_horizontal = Control.SIZE_EXPAND
+	componentName.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	componentName.text = "Component1"
 	
 	var compValue = TextEdit.new()
-	compValue.size_flags_horizontal = Control.SIZE_EXPAND
+	var value : Dictionary = get_edited_object()[get_edited_property()]
+	if (value.has(componentName.text)):
+		print("set val: " + value[componentName.text])
+		compValue.text = value[componentName.text]
+	compValue.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	compValue.connect("text_changed", self, "_on_value_changed", [entry])
 	
 	entry.add_child(componentName)
 	entry.add_child(compValue)
@@ -26,11 +33,16 @@ func _init():
 	add_child(entry)
 	set_bottom_editor(entry)
 	
-#	connect("resized", self, "_on_resized")
+func _on_value_changed(entry):
+	var value : Dictionary = get_edited_object()[get_edited_property()]
+	value[entry.get_child(0).text] = entry.get_child(1).text
+	emit_changed(get_edited_property(), value)
 	
-#set size of components values here
-#func _on_resized():
-#	entry.get_child(1).rect_min_size.x = entry.rect_size.x / 2
+func update_property():
+	var value : Dictionary = get_edited_object()[get_edited_property()]
+	if (value.has(entry.get_child(0).text)):
+		entry.get_child(1).text = value[entry.get_child(0).text]
+		print("update property")
 	
 func _on_expand_pressed():
 	if (entry.visible):
