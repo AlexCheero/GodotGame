a454ffdcd725c205a7099eaf7a151e71c32efb0f
alexcheero <alexcheero157@gmail.com>
2020-04-13 20:13:21 +0300

    another todo

--- Src/Prototype/Prototype/Components/ComponentsMeta.h
+++ Src/Prototype/Prototype/Components/ComponentsMeta.h
@@ -5,6 +5,8 @@ struct ComponentMeta
 {
 	constexpr static const char* name;
 	constexpr static int propertiesCount;
+	//TODO: try to add array of properties names
+	//constexpr static const char* properties[2] = { "a", "b" };
 };
 
 //TODO: separate component name from "Component" to set only its actual name in editor
