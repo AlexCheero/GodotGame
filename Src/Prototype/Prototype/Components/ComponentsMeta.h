34a771c6191c8621fd5f9f3ac92188cfd0b4fe59
alexcheero <alexcheero157@gmail.com>
2020-04-23 00:08:39 +0300

    todo

--- Src/Prototype/Prototype/Components/ComponentsMeta.h
+++ Src/Prototype/Prototype/Components/ComponentsMeta.h
@@ -6,6 +6,8 @@ constexpr size_t GetArraySize(T(&)[N]) { return N; }
 template <typename T>
 struct ComponentMeta {};
 //TODO: separate component name from "Component" to set only its actual name in editor
+//TODO: rename to ComponentSMeta
+//should add type into EntityView::ConstructComponents_impl types list TODO: try to make it automatically
 #define REGISTER_COMPONENT(Type, ...) template<> \
 struct ComponentMeta<Type> \
 { \
