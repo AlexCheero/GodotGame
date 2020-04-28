323e8fb73fae80816bd5b31474700cf750c06c48
alexcheero157 <alexcheero157@gmail.com>
2020-04-29 00:41:40 +0300

    paramNames names removed from HTHAnimSystem anim names used instead

--- Src/Prototype/Prototype/Systems/AnimSystems/HTHAnimSystem.cpp
+++ Src/Prototype/Prototype/Systems/AnimSystems/HTHAnimSystem.cpp
@@ -10,26 +10,14 @@
 //TODO: remove hardcode
 static const char* anims[8] =
 {
-	"Jab Left",
-	"Jab Right",
-	"Hook Left",
-	"Hook Right",
-	"Kick Front Left",
-	"Kick Front Right",
-	"Kick Round Left",
-	"Kick Round Right"
-};
-
-static const char* paramNames[8] =
-{
-	"LeftJab",
-	"RightJab",
-	"LeftHook",
-	"RightHook",
-	"FrontLeftKick",
-	"FrontRightKick",
-	"RoundLeftKick",
-	"RoundRightKick"
+	"Jab_Left",
+	"Jab_Right",
+	"Hook_Left",
+	"Hook_Right",
+	"Kick_Front_Left",
+	"Kick_Front_Right",
+	"Kick_Round_Left",
+	"Kick_Round_Right"
 };
 
 //TODO: don't interrupt previous hit by the next one
@@ -41,21 +29,20 @@ void godot::HTHAnimSystem::operator()(float delta, entt::registry& registry)
 		int prevAnimIdx = attackComp.comboSequenceNum - 1;
 		if (prevAnimIdx < 0)
 			prevAnimIdx = attackComp.comboLength - 1;
-		String prevParamName(paramNames[prevAnimIdx]);
-		String paramName(paramNames[attackComp.comboSequenceNum]);
+		String animName(anims[attackComp.comboSequenceNum]);
+		String prevAnimName(anims[prevAnimIdx]);
 
 		//TODO: same anim time for all animations, maybe use general anim scale for this
 		AnimationPlayer* pAnimPlayer = Object::cast_to<AnimationPlayer>(pAnimTree->get_node(pAnimTree->get_animation_player()));
-		String animName(anims[attackComp.comboSequenceNum]);
 		Ref<Animation> anim = pAnimPlayer->get_animation(animName);
 		//float timeScale = anim->get_length() / attackComp.attackTime;
-		//pAnimTree->set("parameters/" + paramName + "TimeScale/scale", timeScale);
+		//pAnimTree->set("parameters/" + paramName + "_TimeScale/scale", timeScale);
 
 		//TODO: take anim time scale into account
 		registry.assign_or_replace<AttackAnimPlayingComponent>(entity).playBackTimeLeft = anim->get_length();
 
 		//TODO: blend between anims
-		pAnimTree->set("parameters/" + prevParamName + "OneShot/active", false);
-		pAnimTree->set("parameters/" + paramName + "OneShot/active", true);
+		pAnimTree->set("parameters/" + prevAnimName + "_OneShot/active", false);
+		pAnimTree->set("parameters/" + animName + "_OneShot/active", true);
 	});
 }
