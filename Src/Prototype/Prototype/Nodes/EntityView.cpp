f31b66cee3ed92ba1848e22ffd000ab7d7a1085a
alexcheero <alexcheero157@gmail.com>
2020-05-12 15:25:27 +0300

    most of EntityView realization moved to .cpp

---
+++ Src/Prototype/Prototype/Nodes/EntityView.cpp
@@ -0,0 +1,74 @@
+#include "EntityView.h"
+
+template<typename Type, typename... Types>
+void godot::EntityView::ConstructComponents_impl(entt::registry& registry, entt::entity entity)
+{
+	Type component{};
+	if (ConvertToComponent(component))
+		registry.assign<Type>(entity, component);
+	if constexpr (sizeof...(Types))
+		ConstructComponents_impl<Types...>(registry, entity);
+}
+
+template<typename Tag>
+bool godot::EntityView::HasTag()
+{
+	for (int i = 0; i < tags.size(); i++)
+	{
+#ifdef DEBUG
+		//assert that tag's name always ends with "Tag"
+		String debugTagName(TagMeta<Tag>::tagName);
+		ASSERT(debugTagName.find_last("Tag") == debugTagName.length() - 3, "tag's name doesn't ends with \"Tag\"");
+#endif
+		String tagName = String(tags[i]) + "Tag";
+		if (tagName == TagMeta<Tag>::tagName)
+			return true;
+	}
+
+	return false;
+}
+
+template<typename Tag, typename ...Tags>
+void godot::EntityView::ConstructTags_impl(entt::registry& registry, entt::entity entity)
+{
+	if (HasTag<Tag>())
+		registry.assign<Tag>(entity);
+	if constexpr (sizeof...(Tags))
+		ConstructTags_impl<Tags...>(registry, entity);
+}
+
+void godot::EntityView::_register_methods()
+{
+	register_property<EntityView, Dictionary>("components", &EntityView::componentsDict, Dictionary());
+	register_property<EntityView, Array>("tags", &EntityView::tags, Array());
+}
+
+void godot::EntityView::ConstructComponents(entt::registry& registry, entt::entity entity)
+{
+	ConstructComponents_impl
+		<
+			RangedAttackComponent,
+			ThrowableAttackComponent,
+			HealthComponent,
+			GravityComponent,
+			JumpSpeedComponent,
+			SpeedComponent,
+			SimpleFollowComponent,
+			PatrolmanComponent,
+			NavMarginComponent,
+			MeleeWeaponComponent,
+			DecisionMakingComponent
+		>
+		(registry, entity);
+}
+
+void godot::EntityView::ConstructTags(entt::registry& registry, entt::entity entity)
+{
+	ConstructTags_impl
+		<
+			PlayerTag,
+			BotTag,
+			MainCameraTag
+		>
+		(registry, entity);
+}
--- Src/Prototype/Prototype/Nodes/EntityView.h
+++ Src/Prototype/Prototype/Nodes/EntityView.h
@@ -48,11 +48,7 @@ namespace godot
 		}
 
 	public:
-		static void _register_methods()
-		{
-			register_property<EntityView, Dictionary>("components", &EntityView::componentsDict, Dictionary());
-			register_property<EntityView, Array>("tags", &EntityView::tags, Array());
-		}
+		static void _register_methods();
 		void _init() {}
 
 		entt::entity GetEntity() const { return entity; }
@@ -63,109 +59,45 @@ namespace godot
 
 		//TODO: remove script after initing components
 		//TODO: use entity field after merging with EntityHolderNode (merge done) or split it into two separate classes back
-		void ConstructComponents(entt::registry& registry, entt::entity entity)
-		{
-			ConstructComponents_impl
-				<
-					RangedAttackComponent,
-					ThrowableAttackComponent,
-					HealthComponent,
-					GravityComponent,
-					JumpSpeedComponent,
-					SpeedComponent,
-					SimpleFollowComponent,
-					PatrolmanComponent,
-					NavMarginComponent,
-					MeleeWeaponComponent,
-					DecisionMakingComponent
-				>
-				(registry, entity);
-		}
-
-		void ConstructTags(entt::registry& registry, entt::entity entity)
-		{
-			ConstructTags_impl
-				<
-					PlayerTag,
-					BotTag,
-					MainCameraTag
-				>
-				(registry, entity);
-		}
+		void ConstructComponents(entt::registry& registry, entt::entity entity);
+		void ConstructTags(entt::registry& registry, entt::entity entity);
 	};
-
-	template<typename T>
-	inline bool EntityView::ConvertToComponent(T& comp)
-	{
-		String componentKey(ComponentsMeta<T>::typeName);
-		//assert that component's type name always ends with "Component"
-		ASSERT(componentKey.find_last("Component") == componentKey.length() - 9, "component's type name doesn't ends with \"Component\"");
-		componentKey = componentKey.substr(0, componentKey.length() - 9);
-		if (!componentsDict.has(componentKey))
-			return false;
-
-		Dictionary dict = componentsDict[componentKey];
+}
 
 #ifdef DEBUG
-		ASSERT(CheckKeys<T>(dict), "wrong keys in components dict of entity view");
-#endif
-
-		comp = ConvertComponentFromDict<T>(dict);
-		
-		return true;
-	}
-
-#ifdef DEBUG
-	template<typename T>
-	inline bool EntityView::CheckKeys(Dictionary& dict)
-	{
-		if (dict.size() != ComponentsMeta<T>::propertiesCount)
-			return false;
-
-		for (int i = 0; i < ComponentsMeta<T>::propertiesCount; i++)
-		{
-			if (!dict.has(ComponentsMeta<T>::properties[i]))
-				return false;
-		}
-
-		return true;
-	}
-#endif
-
-	template<typename Type, typename... Types>
-	void EntityView::ConstructComponents_impl(entt::registry& registry, entt::entity entity)
-	{
-		Type component{};
-		if (ConvertToComponent(component))
-			registry.assign<Type>(entity, component);
-		if constexpr (sizeof...(Types))
-			ConstructComponents_impl<Types...>(registry, entity);
-	}
-
-	template<typename Tag>
-	inline bool EntityView::HasTag()
-	{
-		for (int i = 0; i < tags.size(); i++)
-		{
-#ifdef DEBUG
-			//assert that tag's name always ends with "Tag"
-			String debugTagName(TagMeta<Tag>::tagName);
-			ASSERT(debugTagName.find_last("Tag") == debugTagName.length() - 3, "tag's name doesn't ends with \"Tag\"");
-#endif
-			String tagName = String(tags[i]) + "Tag";
-			if (tagName == TagMeta<Tag>::tagName)
-				return true;
-		}
-
+template<typename T>
+bool godot::EntityView::CheckKeys(Dictionary& dict)
+{
+	if (dict.size() != ComponentsMeta<T>::propertiesCount)
 		return false;
+
+	for (int i = 0; i < ComponentsMeta<T>::propertiesCount; i++)
+	{
+		if (!dict.has(ComponentsMeta<T>::properties[i]))
+			return false;
 	}
 
-	template<typename Tag, typename ...Tags>
-	inline void EntityView::ConstructTags_impl(entt::registry& registry, entt::entity entity)
-	{
-		if (HasTag<Tag>())
-			registry.assign<Tag>(entity);
-		if constexpr (sizeof...(Tags))
-			ConstructTags_impl<Tags...>(registry, entity);
-	}
+	return true;
+}
+#endif
+
+template<typename T>
+bool godot::EntityView::ConvertToComponent(T& comp)
+{
+	String componentKey(ComponentsMeta<T>::typeName);
+	//assert that component's type name always ends with "Component"
+	ASSERT(componentKey.find_last("Component") == componentKey.length() - 9, "component's type name doesn't ends with \"Component\"");
+	componentKey = componentKey.substr(0, componentKey.length() - 9);
+	if (!componentsDict.has(componentKey))
+		return false;
+
+	Dictionary dict = componentsDict[componentKey];
+
+#ifdef DEBUG
+	ASSERT(CheckKeys<T>(dict), "wrong keys in components dict of entity view");
+#endif
+
+	comp = ConvertComponentFromDict<T>(dict);
+
+	return true;
 }
