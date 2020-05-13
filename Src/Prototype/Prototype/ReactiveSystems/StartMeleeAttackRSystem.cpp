c078c98bb52608bfba8018c3ef26cba700248f6e
alexcheero <alexcheero157@gmail.com>
2020-05-13 16:09:27 +0300

    addition to prev commit

--- Src/Prototype/Prototype/ReactiveSystems/StartMeleeAttackRSystem.cpp
+++ Src/Prototype/Prototype/ReactiveSystems/StartMeleeAttackRSystem.cpp
@@ -7,29 +7,23 @@
 
 namespace //private
 {
+	//TODO0: convert to attack cooldown check
 	void OnInputPressed(entt::registry& registry, entt::entity entity)
 	{
 		if (!registry.has<CurrentWeaponMeleeTag>(entity) || registry.has<AttackAnimPlayingComponent>(entity))
 			return;
 
-		int64_t currTimeMillis = godot::OS::get_singleton()->get_ticks_msec();
-
 		ASSERT(registry.has<MeleeAttackComponent>(entity), "entity has no MeleeAttackComponent");
 
+		int64_t currTimeMillis = godot::OS::get_singleton()->get_ticks_msec();
 		MeleeAttackComponent& attackComp = registry.get<MeleeAttackComponent>(entity);
 		if (attackComp.prevHitTimeMillis + utils::SecondsToMillis(attackComp.GetCurrentHit().attackTime) > currTimeMillis)
-		{
 			registry.remove<AttackPressedTag>(entity);
-			return;
+		else
+		{
+			int64_t millisSinceLastHit = currTimeMillis - attackComp.prevHitTimeMillis;
+			attackComp.prevHitTimeMillis = currTimeMillis;
 		}
-
-		int64_t millisSinceLastHit = currTimeMillis - attackComp.prevHitTimeMillis;
-		attackComp.prevHitTimeMillis = currTimeMillis;
-
-		//TODO0: increment bug, when attack is pressed before animation stops playing
-		//       probably this because of wrong EndAttackAnimSystem update
-		if (millisSinceLastHit <= MeleeAttackComponent::maxComboIntervalMillis)
-			registry.assign<IncrementComboTag>(entity);
 	}
 }
 
